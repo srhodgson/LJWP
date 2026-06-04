1f913c90253e4cb193f6319e6c3dda78ae65d0d8
Stephen <stephen@srhodgson.co.uk>
2026-06-04 22:33:40 +0100

    Finish chapter 01 Signed-off-by: Stephen <stephen@srhodgson.co.uk>

---
+++ src/Chapter01/Exercise03.java
@@ -0,0 +1,10 @@
+package Chapter01;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+
+        System.out.println("The park opens at " + openingHours + " and closes at " + closingHours + ".");
+    }
+}
