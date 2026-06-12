746419a43704fa270472ebd8bb780b86fcf0d58a
Stephen <stephen@srhodgson.co.uk>
2026-06-12 22:59:23 +0100

    Exercise03

---
+++ src/Chapter05/Exercises/Exercise03.java
@@ -0,0 +1,12 @@
+package Chapter05.Exercises;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        int time = 10;
+
+        while (time > 0) {
+            System.out.println("Time left: " + time);
+            time--;
+        }
+    }
+}
