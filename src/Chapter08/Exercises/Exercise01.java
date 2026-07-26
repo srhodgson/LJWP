a083a363192968c4fcc63b63b3d5f00cb019f285
Stephen <stephen@srhodgson.co.uk>
2026-07-26 22:30:25 +0100

    Start the exercises for chapter 8

---
+++ src/Chapter08/Exercises/Exercise01.java
@@ -0,0 +1,16 @@
+package Chapter08.Exercises;
+
+class Dinosaur{
+    String name;
+    int age;
+    String species;
+
+    public Dinosaur(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+}
+
+public class Exercise01 {
+}
